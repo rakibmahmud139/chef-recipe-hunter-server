cd8d7fd8589f7ff4755aa5b19f83d84d7293ea2a
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-20 19:48:03 +0600

    added chef data in server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 // const cors = require('cors');
 const port = process.env.PORT || 5000;
+const chef = require('./data/chef.json');
 
 
 // app.use(cors());
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 })
 
 
+// Chef Data
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
 
 
 
