bad9935378ae82ef6538fd86e933cacf0acc08bd
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-20 23:32:48 +0600

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const chef = require('./data/chef.json');
 
 
-// app.use(cors());
+app.use(cors());
 
 
 
