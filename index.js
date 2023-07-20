f906f1dbe8548549a9d55f147bd30ab31c9994a4
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-20 18:30:43 +0600

    added first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Chef Recipe is running')
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`);
+})
